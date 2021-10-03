3f5bec0844dddcfdff7e920a89e3ab6152a8215f
Szabó Levente <szabo.levente1217@gmail.com>
2021-10-03 20:32:49 +0200

    css kiegészítve, törlés gomb
    css-t javítani, pontosítani kell még
    törlés gomb csak az ikont törli.
    az elvégzett feladatok formázását el kell készíteni

--- scripts.js
+++ scripts.js
@@ -4,14 +4,27 @@ function addToDo() {
         alert("Please write the task in the textfield")
     }else{
         //hozzáadás a listához
-        $("#toDoList").pretend($('<li>' + toDo +'</li>').on('click', function () {
+        $("#toDoList").prepend($('<li>' + toDo + '<span class="deleteIcon"><img src="./icons/delete.svg" class="icon" /></span><span class="tickIcon"><img src="./icons/tick.svg" class="icon" /></span></li>').on("click", function () {
             $(this).toggleClass("checked");
-        }));
+          }));
     };
 }
 
+function isEnter(event) {
+    if (event.key == "Enter") {
+        $("#addButton").click();
+        return false;
+    }
+}
 $(document).ready(function () {
     $("#addButton").on('click', function () {
         addToDo();
+    });
+    $("#toDoList").on('click', '.deleteIcon', function (event) {
+        $(event.target).parent().remove();
+    });
+    $("#toDoList").on('click', 'tickIcon', function (event) {
+        $(event.target).parent().css('text-decoration', 'bold');
     })
-})
+    $("#addToDoContainer input").keypress(isEnter);
+});
